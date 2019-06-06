e98fa9e9359415e8c2ad25a025c4d3a28468daf3
Mikhail <the237th@yandex.com>
2019-06-07 00:20:37 +0300

    Final with some win-1251-charset edits

--- src/vlab/WEB/js/dev/DM_Sets_vlab_1251.js
+++ src/vlab/WEB/js/dev/DM_Sets_vlab_1251.js
@@ -5,8 +5,8 @@ function init_lab() {
         C: [5, 2],
         D: [4, 9, 6],
         expr1: "((A \u222a B) \u222a (C \u22c2 D))",
-        expr2: "((C \u22c2 B) // (A \u22c2 D))",
-        expr3: "((A \u22c2 D) // (C // B))"
+        expr2: "((C \u22c2 B) / (A \u22c2 D))",
+        expr3: "((A \u22c2 D) / (C / B))"
     };
 
     let answers = {
@@ -122,11 +122,11 @@ function init_lab() {
             }
             let content = '' +
                 '<div class = "header">' +
-                '<h1>Операции над множествами</h1>' +
-                '<input type="button" class="btn btn-info" id = "infoModalOpener" value = "Справка">' +
+                '<h1>\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u0438 \u043d\u0430\u0434 \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432\u0430\u043c\u0438</h1>' +
+                '<input type="button" class="btn btn-info" id = "infoModalOpener" value = "' + '\u0421\u043f\u0440\u0430\u0432\u043a\u0430' + '">' +
                 '</div>' +
                 '<div class = "lab-initial">' +
-                '<h2>Исходные данные</h2>' +
+                '<h2>\u0418\u0441\u0445\u043e\u0434\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435</h2>' +
                 '<ul class="list-group">' +
                 '<li class="">A: ' + getSetFormatted(variant.A) + '</li>' +
                 '<li class="">B: ' + getSetFormatted(variant.B) + '</li>' +
@@ -135,7 +135,7 @@ function init_lab() {
                 '</ul>' +
                 '</div>' +
                 '<div class = "lab-task">' +
-                '<h2>Найти</h2>' +
+                '<h2>\u041d\u0430\u0439\u0442\u0438</h2>' +
                 '<ol class="list-group">' +
                 '<li>' + variant.expr1 + '</li>' +
                 '<li>' + variant.expr2 + '</li>' +
@@ -143,16 +143,16 @@ function init_lab() {
                 '</ol>' +
                 '</div>' +
                 '<div class = "lab-answers">' +
-                '<h2>Ответ</h2>' +
+                '<h2>\u041e\u0442\u0432\u0435\u0442</h2>' +
                 '<ul class="list-group">' +
                 '<li>' +
-                '<label for="putAnswer1">Ответ 1: <input type = "text" id = "answer1" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "···" id = "putAnswer1"">' +
+                '<label for="putAnswer1">\u041e\u0442\u0432\u0435\u0442 1: <input type = "text" id = "answer1" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "' + '\xb7\xb7\xb7' + '" id = "putAnswer1"">' +
                 '</li>' +
                 '<li>' +
-                '<label for="putAnswer2">Ответ 2: <input type = "text" id = "answer2" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "···" id = "putAnswer2">' +
+                '<label for="putAnswer2">\u041e\u0442\u0432\u0435\u0442 2: <input type = "text" id = "answer2" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "' + '\xb7\xb7\xb7' + '" id = "putAnswer2">' +
                 '</li>' +
                 '<li>' +
-                '<label for="putAnswer3">Ответ 3: <input type = "text" id = "answer3" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "···" id = "putAnswer3">' +
+                '<label for="putAnswer3">\u041e\u0442\u0432\u0435\u0442 3: <input type = "text" id = "answer3" value = "{}" style="width: 150px;" readonly disabled></label><input class="btn btn-outline-dark" type="button" value = "' + '\xb7\xb7\xb7' + '" id = "putAnswer3">' +
                 '</li>' +
                 '</ul>' +
                 '</div>' +
@@ -174,11 +174,11 @@ function init_lab() {
                 '<td><label>9 <input type="checkbox" id="9"></label></td>' +
                 '</tr>' +
                 '</table>' +
-                '<input class="btn btn-success" type="button" value="Применить" id = "modalShutter">' +
+                '<input class="btn btn-success" type="button" value="' + '\u041f\u0440\u0438\u043c\u0435\u043d\u0438\u0442\u044c' + '" id = "modalShutter">' +
                 '</div>' +
                 '<div class = "info">' +
-                '<h2>Виртуальная лаборатория "Операции над множествами"</h2>' +
-                '<p>Для выполнения задания виртуальной лаборатории необходимо найти значения множеств из блока «Найти» и ввести их в поля для ответа в соответствующем блоке с помощью специальных редакторов множеств. &nbsp; Для редактирования множества, пресдтавляющего ответ, нужно нажать на кнопку "..." и выбрать нужные элементы множества в появивщемся диалоге. Элементы исходных множеств приведены в блоке "Исходные данные". </p>' +
+                '<h2>\u0412\u0438\u0440\u0442\u0443\u0430\u043b\u044c\u043d\u0430\u044f \u043b\u0430\u0431\u043e\u0440\u0430\u0442\u043e\u0440\u0438\u044f "\u041e\u043f\u0435\u0440\u0430\u0446\u0438\u0438 \u043d\u0430\u0434 \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432\u0430\u043c\u0438"</h2>' +
+                '<p>\u0414\u043b\u044f \u0432\u044b\u043f\u043e\u043b\u043d\u0435\u043d\u0438\u044f \u0437\u0430\u0434\u0430\u043d\u0438\u044f \u0432\u0438\u0440\u0442\u0443\u0430\u043b\u044c\u043d\u043e\u0439 \u043b\u0430\u0431\u043e\u0440\u0430\u0442\u043e\u0440\u0438\u0438 \u043d\u0435\u043e\u0431\u0445\u043e\u0434\u0438\u043c\u043e \u043d\u0430\u0439\u0442\u0438 \u0437\u043d\u0430\u0447\u0435\u043d\u0438\u044f \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432 \u0438\u0437 \u0431\u043b\u043e\u043a\u0430 \xab\u041d\u0430\u0439\u0442\u0438\xbb \u0438 \u0432\u0432\u0435\u0441\u0442\u0438 \u0438\u0445 \u0432 \u043f\u043e\u043b\u044f \u0434\u043b\u044f \u043e\u0442\u0432\u0435\u0442\u0430 \u0432 \u0441\u043e\u043e\u0442\u0432\u0435\u0442\u0441\u0442\u0432\u0443\u044e\u0449\u0435\u043c \u0431\u043b\u043e\u043a\u0435 \u0441 \u043f\u043e\u043c\u043e\u0449\u044c\u044e \u0441\u043f\u0435\u0446\u0438\u0430\u043b\u044c\u043d\u044b\u0445 \u0440\u0435\u0434\u0430\u043a\u0442\u043e\u0440\u043e\u0432 \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432. &nbsp; \u0414\u043b\u044f \u0440\u0435\u0434\u0430\u043a\u0442\u0438\u0440\u043e\u0432\u0430\u043d\u0438\u044f \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432\u0430, \u043f\u0440\u0435\u0441\u0434\u0442\u0430\u0432\u043b\u044f\u044e\u0449\u0435\u0433\u043e \u043e\u0442\u0432\u0435\u0442, \u043d\u0443\u0436\u043d\u043e \u043d\u0430\u0436\u0430\u0442\u044c \u043d\u0430 \u043a\u043d\u043e\u043f\u043a\u0443 "\xb7\xb7\xb7" \u0438 \u0432\u044b\u0431\u0440\u0430\u0442\u044c \u043d\u0443\u0436\u043d\u044b\u0435 \u044d\u043b\u0435\u043c\u0435\u043d\u0442\u044b \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432\u0430 \u0432 \u043f\u043e\u044f\u0432\u0438\u0432\u0449\u0435\u043c\u0441\u044f \u0434\u0438\u0430\u043b\u043e\u0433\u0435. \u042d\u043b\u0435\u043c\u0435\u043d\u0442\u044b \u0438\u0441\u0445\u043e\u0434\u043d\u044b\u0445 \u043c\u043d\u043e\u0436\u0435\u0441\u0442\u0432 \u043f\u0440\u0438\u0432\u0435\u0434\u0435\u043d\u044b \u0432 \u0431\u043b\u043e\u043a\u0435 "\u0418\u0441\u0445\u043e\u0434\u043d\u044b\u0435 \u0434\u0430\u043d\u043d\u044b\u0435".</p>' +
                 '</div>';
             $("#jsLab").html(content);
             $("#modal").css("display", "none");
@@ -226,7 +226,7 @@ function init_lab() {
             $("#infoModalOpener").on("click", function () {
                 if ($(".info").css("display").toLowerCase() == "none") {
                     $(".info").css("display", "block");
-                    this.value = "Закрыть";
+                    this.value = "\u0417\u0430\u043a\u0440\u044b\u0442\u044c";
                     $(".lab-initial").css("display", "none");
                     $(".lab-task").css("display", "none");
                     $(".lab-answers").css("display", "none");
@@ -234,7 +234,7 @@ function init_lab() {
                 }
                 else {
                     $(".info").css("display", "none");
-                    this.value = "Справка";
+                    this.value = "\u041d\u0430\u0439\u0442\u0438";
                     $(".lab-initial").css("display", "block");
                     $(".lab-task").css("display", "block");
                     $(".lab-answers").css("display", "block");
